aca8bc2c9a25ead8076d70faa783d43ab9db30a6
blueberry1001 <kubarun317@gmail.com>
2026-06-28 14:36:44 +0900

    すまおｔ

--- Build/cucumvivorWeb.loader.js
+++ Build/cucumvivorWeb.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58018747,
+    wasmFileSize: 58020572,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
